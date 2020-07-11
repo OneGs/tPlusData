805a8f5f1aaee8ec68c972530f128aa4ba9e6036
OneGs <1549522434@qq.com>
2020-07-11 23:22:33 +0800

    getSaleCounts简单测试完成

--- test/tPlus/queryTest.js
+++ test/tPlus/queryTest.js
@@ -10,11 +10,10 @@ const connect = new Connect()
 describe('测试Connect类', function () {
     describe('getSaleCounts、第一次请求获取个数、第二次全部请求', () => {
         it('dataSource.rows.length should equal TotalRecords', async function () {
-            connect.then(async query => {
-                const orders = await query.getSaleCounts({})
-                assert(orders && orders['DataSource']['Rows'])
-                assert(orders['DataSource']['Rows'].length === orders['TotalRecords'])
+            const orders = await connect.then(async query => {
+                return await query.getSaleCounts({})
             })
+            assert(orders['DataSource']['Rows'].length === orders['TotalRecords'])
         });
     })
 })
